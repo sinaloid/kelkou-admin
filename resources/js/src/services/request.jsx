81f7439e978c45aae58d6b54bb878b9efd8e0b68
Sinali Dionou <sinaloid@gmail.com>
2023-11-22 09:32:28 +0000

    request

--- resources/js/src/services/request.jsx
+++ resources/js/src/services/request.jsx
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import { getUser } from './storage';
-import { useContext } from 'react';
 
 export const URL = "https://kelkou.fasodigi.com/"
 //export const URL = "http://127.0.0.1:8000/"
 
-const authCtx = useContext(AppContext);
-const { user } = authCtx;
 
-//const user = getUser()
+
+const user = getUser()
 //console.log(user)
 const request = axios.create({
     baseURL: URL+"api/",
